cd55cf95866518ad1a91317bd8ba446267248cf8
blob03 <maxla2222@protonmail.com>
2021-10-04 22:36:05 +0200

    New tab 'About...'

--- src/controllers/about.js
+++ src/controllers/about.js
@@ -21,10 +21,10 @@ class AboutTab {
 		html += '<div> ' + globalize.translate('AppDesc') + ' <span style="font-weight:400;font-style: italic;" class="aboutcontent">' + packageInfo.description + '</span></div>';
 		html += '<div> ' + globalize.translate('LabelVersion') + ' <span style="font-weight:400;" class="aboutcontent">' + packageInfo.version + '</span></div>';
 		if (browser.tv || layoutManager.tv) {
-			html += '<div> ' + globalize.translate('LabelRepositoryName') + ': <span style="font-weight:400;" class="aboutcontent">' + packageInfo.repository + '</span></div>';
+			html += '<div> ' + globalize.translate('LabelRepositoryName') + ' : <span style="font-weight:400;" class="aboutcontent">' + packageInfo.repository + '</span></div>';
 			html += '<div> ' + globalize.translate('AppContact') + ' <span style="font-weight:400;" class="aboutcontent">' + packageInfo.contact + '</span></div>';
 		} else {
-			html += '<div> ' + globalize.translate('LabelRepositoryName') + ': <span><a is="emby-linkbutton" style="font-weight:400;" rel="noopener noreferrer" class="aboutcontent button-link emby-button" target="_blank" href="' + packageInfo.repository + '">' + packageInfo.repository + '</a></span></div>';
+			html += '<div> ' + globalize.translate('LabelRepositoryName') + ' : <span><a is="emby-linkbutton" style="font-weight:400;" rel="noopener noreferrer" class="aboutcontent button-link emby-button" target="_blank" href="' + packageInfo.repository + '">' + packageInfo.repository + '</a></span></div>';
 			html += '<div> ' + globalize.translate('AppContact') + ' <span><a rel="noopener noreferrer" style="font-weight:400;" class="aboutcontent button-link emby-button" href="mailto:' + packageInfo.contact + '">' + packageInfo.contact + '</a></span></div>';
 		}
 		html += '</div>';
