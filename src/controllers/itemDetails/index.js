ba1f7d58bdd65555a8be41942dc807b1b3aa5ef3
blob03 <maxla2222@protonmail.com>
2022-04-22 19:23:11 +0200

    Translations synchronized with master.\nApplied patch 'fix-item-details-selector'.\nApplied patch 'home-videos'.

--- src/controllers/itemDetails/index.js
+++ src/controllers/itemDetails/index.js
@@ -789,8 +789,7 @@ function renderDetailImage(elem, item, imageLoader) {
     imageLoader.lazyChildren(elem);
 
     // Avoid breaking the design by preventing focus of the poster using the keyboard.
-    if (elem.querySelector('a'))
-		elem.querySelector('a').tabIndex = -1;
+    elem.querySelector('a, button').tabIndex = -1;
 }
 
 function renderImage(page, item) {
