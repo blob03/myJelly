6da38ee12ba9d249ab8082ed6ddfb7eadbbc7946
blob03 <maxla2222@protonmail.com>
2022-06-29 19:21:11 +0200

    Translations synchronized with master.\nApplied patch 'react-router-3'.

--- src/components/appRouter.js
+++ src/components/appRouter.js
@@ -118,7 +118,11 @@ class AppRouter {
                 isBack: action === Action.Pop
             });
         } else {
-            console.warn('[appRouter] "%s" route not found', normalizedPath, location);
+            console.info('[appRouter] "%s" route not found', normalizedPath, location);
+            this.currentRouteInfo = {
+                route: {},
+                path: normalizedPath + location.search
+            };
         }
     }
 
@@ -135,7 +139,7 @@ class AppRouter {
             Events.on(apiClient, 'requestfail', this.onRequestFail);
         });
 
-        ServerConnections.connect().then(result => {
+        return ServerConnections.connect().then(result => {
             this.firstConnectionResult = result;
 
             // Handle the initial route
